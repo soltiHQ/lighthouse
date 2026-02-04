e8aa999de122cf41e1bbf5b515f5d97099d0da95
mr-chelyshkin <argent-keel-0z@icloud.com>
2026-02-04 15:09:09 +0400

    [init] update

--- internal/backend/edge.go
+++ internal/backend/edge.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/soltiHQ/control-plane/domain"
-	"github.com/soltiHQ/control-plane/internal/logctx"
 	"github.com/soltiHQ/control-plane/internal/storage"
 
 	"github.com/rs/zerolog"
@@ -12,8 +11,6 @@ import (
 
 // Discovery is a handler for the discovery service.
 func Discovery(ctx context.Context, logger zerolog.Logger, store storage.Storage, agent *domain.AgentModel) error {
-	logger = logctx.From(ctx, logger)
-
 	if err := store.UpsertAgent(ctx, agent); err != nil {
 		logger.Err(err).
 			Str("agent_id", agent.ID()).
