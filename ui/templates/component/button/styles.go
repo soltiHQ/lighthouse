17f0414e9bf0cbbfa1abbf55264a1e3cdcb2745a
mr-chelyshkin <argent-keel-0z@icloud.com>
2026-02-20 02:41:18 +0400

    [ui] re-struct components

--- ui/templates/component/button/styles.go
+++ ui/templates/component/button/styles.go
@@ -1,5 +1,21 @@
 package button
 
+import "github.com/a-h/templ"
+
+// mergeAttrs flattens a variadic list of templ.Attributes into one map.
+func mergeAttrs(attrs []templ.Attributes) templ.Attributes {
+	if len(attrs) == 0 {
+		return nil
+	}
+	out := templ.Attributes{}
+	for _, a := range attrs {
+		for k, v := range a {
+			out[k] = v
+		}
+	}
+	return out
+}
+
 func radiusFor(v Variant) string {
 	if v == VariantPrimary {
 		return "rounded-[var(--r-6)] "
